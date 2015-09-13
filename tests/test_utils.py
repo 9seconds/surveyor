0639146eb443c3c4c6fb49b8cb8cda289d74add6
9seconds <nineseconds@yandex.ru>
2015-09-13 15:46:08 +0300

    Add unittests for utils

---
+++ tests/test_utils.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+
+import pytest
+
+import surveyor.utils as utils
+
+
+@pytest.mark.parametrize("value", (
+    "1",
+    "y",
+    "yes",
+    "true",
+    "True",
+    "Yes",
+    "YES",
+    "TRUE",
+    "ON",
+    "on"
+))
+def test_strtobool_true(value):
+    assert utils.strtobool(value)
+
+
+@pytest.mark.parametrize("value", (
+    "0",
+    "n",
+    "no",
+    "false",
+    "False",
+    "FALSE",
+    "Off",
+    "off",
+    "NO",
+    "N",
+    None
+))
+def test_strtobool_false(value):
+    assert not utils.strtobool(value)
+
+
+@pytest.mark.parametrize("value", (
+    [1],
+    {1: 1},
+))
+def test_strtobool_unknown(value):
+    with pytest.raises(ValueError):
+        utils.strtobool(value)
+
+
+@pytest.mark.parametrize("value, result", (
+    ("1", 1),
+    ("-1", -1),
+    ("0", 0),
+    ("0.1", 0.1),
+    ("-0.1", -0.1),
+    (".1", 0.1),
+    ("1.", 1.0),
+    ("", ""),
+    ("text", "text"),
+    ("1text", "1text"),
+    ("2 text", "2 text"),
+    ("1.0 text", "1.0 text")
+))
+def test_guess_text_int(value, result):
+    assert utils.guess_text(value) == result
