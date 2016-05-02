8706bff5463c908d00ef0f10dd870899caf3fc8b
9seconds <nineseconds@yandex.ru>
2016-05-02 11:24:18 +0300

    Move to boring3 template
    Boring3 sits here:
    https://github.com/9seconds/boring3

--- setup.py
+++ setup.py
@@ -3,79 +3,15 @@
 
 
 import setuptools
-import setuptools.command.test
-
-import surveyor
 
 
-with open("README.rst", "r") as resource:
-    LONG_DESCRIPTION = resource.read()
-
-
-# copypasted from http://pytest.org/latest/goodpractises.html
-# noinspection PyAttributeOutsideInit
-class PyTest(setuptools.command.test.test):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        setuptools.command.test.initialize_options(self)
-        self.pytest_args = None  # pylint: disable=W0201 noqa
-
-    def finalize_options(self):
-        setuptools.command.test.finalize_options(self)
-        self.test_args = []  # pylint: disable=W0201 noqa
-        self.test_suite = True  # pylint: disable=W0201 noqa
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-
-        import pytest
-        import sys
-
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
+try:
+    import multiprocessing
+    assert multiprocessing
+except ImportError:
+    pass
 
 
 setuptools.setup(
-    name="surveyor",
-    description="Painless XLSX templating",
-    long_description=LONG_DESCRIPTION,
-    version=".".join(map(str, surveyor.__version__)),
-    author="Sergey Arkhipov",
-    license="MIT",
-    author_email="serge@aerialsounds.org",
-    maintainer="Sergey Arkhipov",
-    maintainer_email="serge@aerialsounds.org",
-    url="https://github.com/9seconds/surveyor/",
-    entry_points={
-        'console_scripts': ['surveyor=surveyor:main'],
-    },
-    install_requires=[
-        "openpyxl>=2.2,<2.3",
-        "six"
-    ],
-    extras_require={
-        'fast':  ["lxml"],
-    },
-    keywords="excel template spreadsheet",
-    tests_require=[
-        "pytest>=2.6.1"
-    ],
-    packages=setuptools.find_packages(),
-    cmdclass={'test': PyTest},
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Office/Business :: Financial :: Spreadsheet",
-
-    ],
-    zip_safe=False
-)
+    setup_requires=["pbr>=1.8"],
+    pbr=True)
